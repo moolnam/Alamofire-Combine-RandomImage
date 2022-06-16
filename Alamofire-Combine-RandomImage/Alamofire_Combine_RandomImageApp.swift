0b5d1f42f27526abe25c403a4cc073d3badef458
moolnam <moolnam>
2022-06-16 19:19:07 +0900

    Initial Commit

---
+++ Alamofire-Combine-RandomImage/Alamofire_Combine_RandomImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alamofire_Combine_RandomImageApp.swift
+//  Alamofire-Combine-RandomImage
+//
+//  Created by KimJongHee on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct Alamofire_Combine_RandomImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alamofire-Combine-RandomImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alamofire-Combine-RandomImage
+//
+//  Created by KimJongHee on 2022/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
